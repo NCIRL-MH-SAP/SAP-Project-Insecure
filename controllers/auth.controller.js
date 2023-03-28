e50ad7bbcf0c9a5074bb3bdb0c44428e7156e3f5
Maxi <herreramaxi@gmail.com>
2023-03-28 01:17:39 +0100

    addin insecure login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,40 +3,66 @@ const User = db.User;
 const { Sequelize } = require('sequelize');
 const Op = Sequelize.Op;
 
-exports.signIn = (req, res) => {
+exports.signIn = async (req, res) => {
     if (!(req.body.email && req.body.password)) {
         return res.status(400).send({ message: "Mandatory fields not provided" });
     }
 
-    User.findOne({
-        where: {
-            email: { [Op.iLike]: req.body.email }
-        }
-    })
-        .then(user => {
-            if (!user) {
-                return res.status(404).send({ message: "Invalid user credential" });
-            }
+    userRows = await db.sequelize.query('SELECT id, "firstName", "lastName", "email", "password" FROM public."Users" where "Users".email =\'' + req.body.email + '\' limit 1;', {
+        type: db.sequelize.QueryTypes.SELECT
+    });
 
-            var passwordIsValid = req.body.password === user.password;
+    if (userRows.length == 1) {       
+        const user = userRows[0];
+        var passwordIsValid = req.body.password === user.password;
 
-            if (!passwordIsValid) {
-                return res.status(401).send({
-                    accessToken: null,
-                    message: "Invalid user credential"
-                });
-            }
-            res.send({
-                id: user.id,
-                username: user.firstName + " " + user.lastName,
-                firstName: user.firstName,
-                lastName: user.lastName,
-                email: user.email
+        if (!passwordIsValid) {
+            return res.status(401).send({
+                accessToken: null,
+                message: "Invalid user credential"
             });
-        })
-        .catch(err => {
-            res.status(500).send({ message: err.message });
+        }
+
+        return res.send({
+            id: user.id,
+            username: user.firstName + " " + user.lastName,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email
         });
+    }
+
+    return res.status(404).send({ message: "Invalid user credential" });
+
+    // User.findOne({
+    //     where: {
+    //         email: { [Op.iLike]: req.body.email }
+    //     }
+    // })
+    //     .then(user => {
+    //         if (!user) {
+    //             return res.status(404).send({ message: "Invalid user credential" });
+    //         }
+
+    //         var passwordIsValid = req.body.password === user.password;
+
+    //         if (!passwordIsValid) {
+    //             return res.status(401).send({
+    //                 accessToken: null,
+    //                 message: "Invalid user credential"
+    //             });
+    //         }
+    //         res.send({
+    //             id: user.id,
+    //             username: user.firstName + " " + user.lastName,
+    //             firstName: user.firstName,
+    //             lastName: user.lastName,
+    //             email: user.email
+    //         });
+    //     })
+    //     .catch(err => {
+    //         res.status(500).send({ message: err.message });
+    //     });
 };
 
 exports.signUp = (req, res) => {
