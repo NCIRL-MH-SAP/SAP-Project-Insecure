7e81fc13deb305c0184ee6b60cfec69d0fb2de18
Maxi <herreramaxi@gmail.com>
2023-04-01 10:04:18 +0100

    add Content-Security-Policy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private meta: Meta) { }
+
+  ngOnInit(): void {
+    if (environment.production) {
+      this.meta.addTag({
+        name: "http-equiv=Content-Security-Policy", content: `default-src 'none';
+              frame-src 'self';
+              script-src 'self';
+              style-src 'self' 'unsafe-inline';
+              font-src 'self';
+              img-src 'self' data:;
+              connect-src 'self'`});
+    }
+  }
   title = 'mh-sap-project';
 }
